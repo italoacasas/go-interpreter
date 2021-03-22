a272269e94bd48268e02b0be026abb98a11b957b
Italo A. Casas <me@italoacasas.com>
2021-03-22 12:20:15 +0100

    removing typo

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package goiterpreter
+package main
 
 import (
 	"fmt"
@@ -13,7 +13,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+	fmt.Printf("Hello %s! This is a Golan Interpreter!\n",
 		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
